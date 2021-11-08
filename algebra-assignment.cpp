3445c5e3a6bce2102afce0a68c921c165caaa770
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-11-08 11:35:35 +0900

    Update algebra-assignment.cpp

--- algebra-assignment.cpp
+++ algebra-assignment.cpp
@@ -1,20 +1,30 @@
 // Make the square matrix into an upper triangle form
 // input n*n matrix
 // using three operation
+/*
+Matrix that succeded in changing to upper triangle.
+size : 3
+1 -1 1 4 2 1 9 3 1
+*/
 #include<iostream>
 #include<vector>
 using namespace std;
 int errorRowInCheck, errorColInCheck;
-vector<double> saveRow;
+vector<long long> saveRow;
 // Interchanging any two equations.
 int gcd(int a, int b) {
-	if (b == 0) return a;
-	else return gcd(b, a % b);
+	int c;
+	while (b != 0) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
 }
 int lcm(int a, int b) {
-	return a / b * gcd(a, b);
+	return a * b / gcd(a, b);
 }
-void operationOne(vector<vector<double>> &matrix, int matrixNSize, int rowOne, int rowTwo) {
+void operationOne(vector<vector<long long>> &matrix, int matrixNSize, int rowOne, int rowTwo) {
 	for (int col = 0; col < matrixNSize; col++) {
 		double tmp = matrix[rowOne][col];
 		matrix[rowOne][col] = matrix[rowTwo][col];
@@ -22,45 +32,39 @@ void operationOne(vector<vector<double>> &matrix, int matrixNSize, int rowOne, i
 	}
 };
 // Multiplying any equation by a nonzero constant.
-void operationTwo(vector<double> &matrix, int matrixNSize, int multipleNum) {
+void operationTwo(vector<long long> &matrix, int matrixNSize, int multipleNum) {
 	for (int col = 0; col < matrixNSize; col++) {
 		matrix[col] *= multipleNum;
 	}
 }
 // Adding a multiple of one equation to another.
-void operationThree(vector<double> &matrix, vector<double> &matrixPlus, int matrixNSize) {
+void operationThree(vector<long long> &matrix, vector<long long> &matrixPlus, int matrixNSize) {
 	for (int col = 0; col < matrixNSize; col++) {
 		matrix[col] += matrixPlus[col];
 	}
 }
 // Check upper triangle
-bool checkUpperTriangle(vector<vector<double>> &matrix, int matrixNSize) {
+bool checkUpperTriangle(vector<vector<long long>> &matrix, int matrixNSize) {
 	for (int row = 0; row < matrixNSize; row++) {
-		// upper triangle에서 0이 아닌 자리에 0이 들어간 경우.
-		for (int col = row; col < matrixNSize; col++) {
-			if (matrix[row][col] == 0)
-				return false;
-		}
-		// upper triangle에서 0인 자리에 0이 아닌 수가 들어간 경우.
-		for (int col = 0; col < row; col++)
-			if (matrix[row][col] != 0) {
+		for (int col = 0; col < row; col++) {
+			if (matrix[row][col] != 0)
 				return false;
 		}
 	}
 	return true;
 }
 // Print matrix
-void printMatrix(vector<vector<double>> matrix, int matrixSize) {
+void printMatrix(vector<vector<long long>> matrix, int matrixSize) {
 	for (int i = 0; i < matrixSize; i++) {
 		for (int j = 0; j < matrixSize; j++)
 			cout << matrix[i][j] << " ";
 		cout << "\n";
 	}
 }
-void saveMatrixOriginalRow(vector<double>& matrix) {
+void saveMatrixOriginalRow(vector<long long>& matrix) {
 	saveRow = matrix;
 }
-void setMatrixOriginalRow(vector<double>& matrix) {
+void setMatrixOriginalRow(vector<long long>& matrix) {
 	matrix = saveRow;
 }
 bool checkDifferentSign(int a, int b) {
@@ -71,7 +75,7 @@ int main() {
 	int matrixNSize;
 	int inputNum;
 	cin >> matrixNSize;
-	vector<vector<double>> matrix(matrixNSize, vector<double>(matrixNSize, 0));
+	vector<vector<long long>> matrix(matrixNSize, vector<long long>(matrixNSize, 0));
 	for (int row = 0; row < matrixNSize; row++) {
 		for (int col = 0; col < matrixNSize; col++) {
 			cin >> inputNum;
@@ -86,9 +90,15 @@ int main() {
 				saveMatrixOriginalRow(matrix[col]);
 				int LCM = lcm(abs(matrix[row][col]), abs(matrix[col][col]));
 				// 비교 row
-				operationTwo(matrix[col], matrixNSize, ((checkDifferentSign(matrix[row][col], matrix[col][col])) ? LCM / (abs(matrix[col][col])) : (LCM / (abs(matrix[col][col]) * -1))));
+				bool differentSign = checkDifferentSign(matrix[row][col], matrix[col][col]);
+				if (differentSign) {
+					operationTwo(matrix[col], matrixNSize, (LCM / (abs(matrix[col][col]))));
+				}
+				else {
+					operationTwo(matrix[col], matrixNSize, (LCM / (abs(matrix[col][col]) * -1)));
+				}
 				// 변경 row
-				operationTwo(matrix[row], matrixNSize, LCM / (abs(matrix[col][col])));
+				operationTwo(matrix[row], matrixNSize, LCM / (abs(matrix[row][col])));
 				operationThree(matrix[row], matrix[col], matrixNSize);
 				setMatrixOriginalRow(matrix[col]);
 			}
