4b0038630b09e87433ff4eabddd16d44da388a7a
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-11-08 15:50:43 +0900

    Using lcm, but fail

--- algebra-assignment.cpp
+++ algebra-assignment.cpp
@@ -1,17 +1,40 @@
-// Make the square matrix into an upper triangle form
-// input n*n matrix
-// using three operation
-/*
-Matrix that succeded in changing to upper triangle.
-size : 3
-1 -1 1 4 2 1 9 3 1
-*/
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int errorRowInCheck, errorColInCheck;
-vector<long long> saveRow;
-// Interchanging any two equations.
+int matrixNSize;
+// operation 1
+void changeRow(vector<vector<double>>& matrix, int row, int curRow) {
+	vector<double> saveRow;
+	saveRow = matrix[curRow];
+	matrix[curRow] = matrix[row];
+	matrix[row] = saveRow;
+}
+void checkHaveToChangeRow(vector<vector<double>>& matrix, int checkRow) {
+	bool correct = false;
+	for (int row = 0; row < matrixNSize; row++) {
+		int zeroCnt = 0;
+		for (int col = 0; col <= row; col++)
+			if (matrix[row][col] == 0) zeroCnt++;
+		if (zeroCnt == checkRow) {
+			correct = true;
+			changeRow(matrix, row, checkRow);
+		}
+		if (correct) break;
+	}
+}
+// operation 2
+void rowMultipledByNum(vector<double>& matrix, int multipleNum) {
+	for (int col = 0; col < matrixNSize; col++) {
+		matrix[col] *= multipleNum;
+	}
+}
+// operation 3
+void rowAddedRow(vector<double> &matrix, vector<double> &matrixPlus) {
+	for (int col = 0; col < matrixNSize; col++) {
+		matrix[col] += matrixPlus[col];
+	}
+}
+// get gcd
 int gcd(int a, int b) {
 	int c;
 	while (b != 0) {
@@ -21,30 +44,12 @@ int gcd(int a, int b) {
 	}
 	return a;
 }
+// get lcm
 int lcm(int a, int b) {
-	return a * b / gcd(a, b);
-}
-void operationOne(vector<vector<long long>> &matrix, int matrixNSize, int rowOne, int rowTwo) {
-	for (int col = 0; col < matrixNSize; col++) {
-		double tmp = matrix[rowOne][col];
-		matrix[rowOne][col] = matrix[rowTwo][col];
-		matrix[rowTwo][col] = tmp;
-	}
-};
-// Multiplying any equation by a nonzero constant.
-void operationTwo(vector<long long> &matrix, int matrixNSize, int multipleNum) {
-	for (int col = 0; col < matrixNSize; col++) {
-		matrix[col] *= multipleNum;
-	}
-}
-// Adding a multiple of one equation to another.
-void operationThree(vector<long long> &matrix, vector<long long> &matrixPlus, int matrixNSize) {
-	for (int col = 0; col < matrixNSize; col++) {
-		matrix[col] += matrixPlus[col];
-	}
+	return (a * b) / gcd(a, b);
 }
 // Check upper triangle
-bool checkUpperTriangle(vector<vector<long long>> &matrix, int matrixNSize) {
+bool checkUpperTriangle(vector<vector<double>> &matrix) {
 	for (int row = 0; row < matrixNSize; row++) {
 		for (int col = 0; col < row; col++) {
 			if (matrix[row][col] != 0)
@@ -53,58 +58,53 @@ bool checkUpperTriangle(vector<vector<long long>> &matrix, int matrixNSize) {
 	}
 	return true;
 }
-// Print matrix
-void printMatrix(vector<vector<long long>> matrix, int matrixSize) {
-	for (int i = 0; i < matrixSize; i++) {
-		for (int j = 0; j < matrixSize; j++)
-			cout << matrix[i][j] << " ";
-		cout << "\n";
-	}
-}
-void saveMatrixOriginalRow(vector<long long>& matrix) {
-	saveRow = matrix;
-}
-void setMatrixOriginalRow(vector<long long>& matrix) {
-	matrix = saveRow;
-}
 bool checkDifferentSign(int a, int b) {
 	if ((a > 0 && b < 0) || (a < 0 && b > 0)) return true;
 	else return false;
 }
+// Print matrix
+void printMatrix(vector<vector<double>> &matrix) {
+	for (int i = 0; i < matrixNSize; i++) {
+		for (int j = 0; j < matrixNSize; j++) {
+			cout << fixed;
+			cout.precision(10);
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
 int main() {
-	int matrixNSize;
-	int inputNum;
+	bool upperTriangle = true;
 	cin >> matrixNSize;
-	vector<vector<long long>> matrix(matrixNSize, vector<long long>(matrixNSize, 0));
+	vector<vector<double>> matrix(matrixNSize, vector<double>(matrixNSize, 0));
 	for (int row = 0; row < matrixNSize; row++) {
 		for (int col = 0; col < matrixNSize; col++) {
-			cin >> inputNum;
+			double inputNum;  cin >> inputNum;
 			matrix[row][col] = inputNum;
 		}
 	}
-	saveMatrixOriginalRow(matrix[0]);
 	for (int row = 1; row < matrixNSize; row++) {
+		checkHaveToChangeRow(matrix, row);
 		for (int col = 0; col < row; col++) {
-			if (matrix[row][col] != 0) {
-				// 비교 row를 변경하면 원래 row로 되돌려줄 row 저장.
-				saveMatrixOriginalRow(matrix[col]);
-				int LCM = lcm(abs(matrix[row][col]), abs(matrix[col][col]));
-				// 비교 row
-				bool differentSign = checkDifferentSign(matrix[row][col], matrix[col][col]);
-				if (differentSign) {
-					operationTwo(matrix[col], matrixNSize, (LCM / (abs(matrix[col][col]))));
-				}
-				else {
-					operationTwo(matrix[col], matrixNSize, (LCM / (abs(matrix[col][col]) * -1)));
-				}
-				// 변경 row
-				operationTwo(matrix[row], matrixNSize, LCM / (abs(matrix[row][col])));
-				operationThree(matrix[row], matrix[col], matrixNSize);
-				setMatrixOriginalRow(matrix[col]);
-			}
+			vector<double> save = matrix[col];
+			int LCM = lcm(abs(matrix[row][col]), abs(matrix[col][col]));
+			int GCD = gcd(abs(matrix[row][col]), abs(matrix[col][col]));
+			rowMultipledByNum(matrix[row], LCM / matrix[row][col]);
+			if (!checkDifferentSign(matrix[row][col], matrix[col][col]))
+				LCM *= -1;
+			rowMultipledByNum(matrix[col], LCM / matrix[col][col]);
+			rowAddedRow(matrix[row], matrix[col]);
+			matrix[col] = save;
 		}
+		printMatrix(matrix);
+		if (checkUpperTriangle(matrix)) {
+			upperTriangle = true;
+			break;
+		}
+		else upperTriangle = false;
 	}
-	printMatrix(matrix, matrixNSize);
-	if(checkUpperTriangle(matrix, matrixNSize)) cout << "Success\n";
-	else cout << "Fail\n";
+	if (upperTriangle) printMatrix(matrix);
+	else cout << "Can not chage upper triangle form" << "\n";
 }
