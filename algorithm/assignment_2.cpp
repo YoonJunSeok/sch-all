b4b39597341fba2dc0a14657e6c743751f0bf19b
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-11-20 12:28:37 +0900

    Run_Error
    100 in-out success, but run error

--- algorithm/assignment_2.cpp
+++ algorithm/assignment_2.cpp
@@ -3,10 +3,7 @@
 #include <algorithm>
 #define MAX_SIZE 10000
 using namespace std;
-// 인접 연결 리스트.
-// vertex[vertexNumber]은 연결되어있는 간선과 그 간선의 가중치를 pair로 가지고 있다.
-vector<pair<int, int>> vertex[MAX_SIZE];
-// vector number 저장
+vector<pair<int, int>> adj[MAX_SIZE];
 vector<int> vertexNumber;
 class PriorityQueue {
 public: 
@@ -30,40 +27,36 @@ public:
 	}
 
 	void push(pair<int, int> info) {
-		// store vertex number
+		// 새로운 간선은 맨 마지막에 입력.
 		++heapSize;
 		minHeap[heapSize].first = info.first;
-		// stroe edge weight
 		minHeap[heapSize].second = info.second;
+		bool sameVertex = false;
 		for (int i = 1; i < heapSize; i++) {
-			// same vertex, different weight
+			// 새로 들어온 간선과 기존에 있는 간선이 겹치는지 확인.
+			// same vertex
 			if (minHeap[i].first == minHeap[heapSize].first) {
+				sameVertex = true;
+				// old weight > new weight
+				// 새로운 간선으로 교체한 후 기존 간선은 삭제한다.
 				if (minHeap[i].second > minHeap[heapSize].second) {
 					swap(i, heapSize);
 					minHeap[heapSize].first = 0;
 					minHeap[heapSize].second = 0;
 					heapSize--;
+					updateUp(i);
 				}
+				// 기존 간선을 유지하고 새로운 간선은 삭제한다.
 				else {
 					minHeap[heapSize].first = 0;
 					minHeap[heapSize].second = 0;
 					heapSize--;
 				}
-			}
-		}
-		for (int i = heapSize; i > 1; i /= 2) {
-			if (minHeap[i / 2].second > minHeap[i].second)
-				swap(i / 2, i);
-			else if (minHeap[i/2].second < minHeap[i].second)
 				break;
-			else if (minHeap[i/2].second == minHeap[i].second) {
-				// same weight, small vertex over big vertex
-				if (minHeap[i/2].first > minHeap[i].first) {
-					swap(i/2, i);
-				}
 			}
 		}
-		update();
+		if(!sameVertex)
+			updateUp(heapSize);
 	}
 
 	void pop() {
@@ -74,29 +67,68 @@ public:
 			minHeap[heapSize].first = 0;
 			minHeap[heapSize].second = 0;
 			heapSize--;
-			update();
+			updateDown();
 		}
 	}
 
-	void update() {
-		// 최대의 자리를 min heap에 맞게 변경한다.
-		for (int i = 1; i * 2 <= heapSize; i++) {
-			if (minHeap[i * 2 + 1].first != 0) {
-				if (minHeap[i].second < minHeap[i * 2].second && minHeap[i].second < minHeap[i * 2 + 1].second) break;
-				else if (minHeap[i * 2].second < minHeap[i * 2 + 1].second) {
-					swap(i, i * 2);
-					i = i * 2;
+	// 루트에 있는 정점의 올바른 자리 탐색.
+	// pop 하여 높은 weight의 정점이 루트에 갔을 때 사용.
+	void updateDown() {
+		int cur = 1;
+		int leftChild = cur * 2;
+		int rightChild = cur * 2 + 1;
+		// 자식이 2명인 경우.
+		while (minHeap[leftChild].first != 0 && minHeap[rightChild].first != 0) {
+			if (minHeap[leftChild].second <= minHeap[rightChild].second) {
+				if (minHeap[cur].second > minHeap[leftChild].second) {
+					swap(leftChild, cur);
+					cur = leftChild;
 				}
-				else if (minHeap[i * 2].second > minHeap[i * 2 + 1].second) {
-					swap(i, i * 2 + 1);
-					i = i * 2 + 1;
-				}
-				else if (minHeap[i * 2].second == minHeap[i * 2 + 1].second) {
-					if (minHeap[i * 2].first > minHeap[i * 2 + 1].first)
-						swap(i, i * 2 + 1);
-					else swap(i, i * 2);
-				}
+				else break;
 			}
+			else {
+				if (minHeap[cur].second > minHeap[rightChild].second) {
+					swap(rightChild, cur);
+					cur = rightChild;
+				}
+				else break;
+			}
+			leftChild = cur * 2;
+			rightChild = cur * 2 + 1;
+		}
+		while (minHeap[leftChild].first != 0) {
+			if (minHeap[cur].second > minHeap[leftChild].second) {
+				swap(leftChild, cur);
+				cur = leftChild;
+			}
+			else break;
+			leftChild = cur * 2;
+		}
+	}
+
+	// 해당 정점부터 루트까지 올바른 자리 탐색.
+	// push 하여 낮은 weight이 마지막에 존재할 때 사용.
+	void updateUp(int cur) {
+		int parent = cur / 2;
+		while (parent >= 1) {
+			// 일반적인 경우.
+			// 현재 weight이 부모 weight보다 낮은 경우.
+			if (minHeap[cur].second < minHeap[parent].second) {
+				swap(cur, parent);
+				cur = parent;
+				parent = cur / 2;
+			}
+			// 특별한 경우.
+			// 현재 weight과 부모 weight이 같지만 정점의 우선순위가 현재 정점이 높은 경우.
+			else if (minHeap[cur].second == minHeap[parent].second) {
+				if (minHeap[cur].first < minHeap[parent].first) {
+					swap(cur, parent);
+					cur = parent;
+					parent = cur / 2;
+				}
+				else break;
+			}
+			else break;
 		}
 	}
 
@@ -111,14 +143,10 @@ public:
 };
 void prim(PriorityQueue pq, int startVertex) {
 	bool check[MAX_SIZE] = { false, };
-	// start vertex is tree
-	// if cehck[vertexNumber] = false then fringe.
 	check[startVertex] = true;
 	int sumMinWeight = 0;
-	// base case
-	for (int i = 0; i < vertex[startVertex].size(); i++) {
-		pq.push(make_pair(vertex[startVertex][i].first, vertex[startVertex][i].second));
-		check[vertex[startVertex][i].first] = false;
+	for (int i = 0; i < adj[startVertex].size(); i++) {
+		pq.push(make_pair(adj[startVertex][i].first, adj[startVertex][i].second));
 	}
 	vertexNumber.push_back(startVertex);
 
@@ -129,12 +157,11 @@ void prim(PriorityQueue pq, int startVertex) {
 			check[p.first] = true;
 			sumMinWeight += p.second;
 
-			for (int j = 0; j < vertex[p.first].size(); j++) {
-				if (!check[vertex[p.first][j].first])
-					pq.push(make_pair(vertex[p.first][j].first, vertex[p.first][j].second));
+			for (int j = 0; j < adj[p.first].size(); j++) {
+				if (!check[adj[p.first][j].first])
+					pq.push(make_pair(adj[p.first][j].first, adj[p.first][j].second));
 			}
 
-			// 최소의 weight 값과 vertex number 값을 가져온다.
 			vertexNumber.push_back(p.first);
 		}
 	}
@@ -151,8 +178,8 @@ int main() {
 		int source, destination, weight;
 		cin >> source >> destination >> weight;
 		// source에 해당하는 vertex는 weight을 가지고 있는 간선을 가지고 destinationo과 연결되어 있다.
-		vertex[source].push_back(make_pair(destination, weight));
-		vertex[destination].push_back(make_pair(source, weight));
+		adj[source].push_back(make_pair(destination, weight));
+		adj[destination].push_back(make_pair(source, weight));
 	}
 	for (int i = 0; i < questionCnt; i++) {
 		PriorityQueue pq;
