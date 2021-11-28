5725e02905b84371df7e1a21cc7fd6a681e17ea2
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-11-28 14:05:07 +0900

    get eigenVectors

--- algebra/assignment_2.cpp
+++ algebra/assignment_2.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
-#define matrix vector<vector<double>>
-#define row vector<double>
-int systemA[16] = { 1,2,4,7,0,3,5,8,0,0,6,9,0,0,0,10 };
-row ramda;
-matrix AMinusRamdaIdentity[4];
-matrix eigenVector[4];
+#define R 't';
 using namespace std;
+typedef vector<vector<double>> matrix;
+typedef vector<double> row;
+int cnt = 3;
+int s[16] = { 1,2,4,7,0,3,5,8,0,0,6,9,0,0,0,10 };
+double eigenVector[4][4] = { {1,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,1} };
+row ramda;
+matrix systemA(4, row(4, 0));
+matrix AMinusRamdaIdentity(4, row(4,0));
 // for echelon
 int non_zero(row& r) {
 	for (int i = 0; i < r.size(); i++) {
@@ -15,7 +18,7 @@ int non_zero(row& r) {
 	}
 	return -1;
 }
-void print(matrix& mat) {
+void matrixPrint(matrix& mat) {
 	cout << "-----------------\n";
 	for (int i = 0; i < mat.size(); i++) {
 		for (auto& iter : mat[i]) {
@@ -26,12 +29,12 @@ void print(matrix& mat) {
 }
 void operOne(matrix& mat, int i, double k) {
 	for (auto& iter : mat[i]) {
-		iter = k * iter;
+		iter = round(k * iter * 10) / 10;
 	}
 }
 void operTwo(matrix& mat, int n, double k, int j) {
 	for (int i = 0; i < mat[0].size(); ++i) {
-		mat[j][i] += k * mat[n][i];
+		mat[j][i] += round(k * mat[n][i] * 10) / 10;
 	}
 }
 void echelon(matrix& mat) {
@@ -64,18 +67,99 @@ void rowEchelon(matrix& mat) {
 		}
 	}
 }
-int main() {
-	matrix input;
-	int k;
+// calculator
+void setSystem() {
+	int cnt = 0;
 	for (int i = 0; i < 4; i++) {
-		row r;
 		for (int j = 0; j < 4; j++) {
-			double tmp;
-			cin >> tmp;
-			r.push_back(tmp);
+			systemA[i][j] = s[cnt];
+			cnt++;
 		}
-		input.push_back(r);
 	}
-	cin >> k;
-	rowEchelon(input);
+}
+void setRamda() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (i == j) {
+				ramda.push_back(systemA[i][j]);
+			}
+		}
+	}
+}
+void setAMinusRamdaIdentity(int ramda) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (i == j) {
+				int eigenValue = systemA[i][j] - ramda;
+				AMinusRamdaIdentity[i][j] = eigenValue;
+			}
+			else AMinusRamdaIdentity[i][j] = systemA[i][j];
+		}
+	}
+}
+void calEigenVector(matrix& mat) {
+	// 위치와 그 위치에 해당하는 값.
+	vector<pair<int, double>> equation;
+	for (int i = 3; i >= 0; i--) {
+		for (int j = 0; j < 4; j++) {
+			// Xj 값이 있는지 확인.
+			if (mat[i][j] != 0) {
+				// 행에 해당하는 값 저장.
+				equation.push_back(make_pair(j, mat[i][j]));
+			}
+		}
+		// 행의 값이 모두 0이라면 그 행번째 x의 값은 1이다.
+		// 실제로는 임의의 실수가 모두 해당될 수 있지만 계산의 편의성을 위한다.
+		if (equation.size() == 0) {
+			eigenVector[cnt][i] = 1;
+		}
+		// 행에 0이 아닌 값이 하나라면 그에 해당하는 x값은 0일수밖에 없다.
+		else if (equation.size() == 1) {
+			eigenVector[cnt][equation[0].first] = 0;
+		}
+		else if (equation.size() == 2) {
+			// reduced row echelon form에서 1인 곳.
+			if (equation[0].second == 1) {
+				eigenVector[cnt][equation[0].first] = equation[1].second * -1;
+				eigenVector[cnt][equation[1].first] = 1;
+			}
+		}
+		equation.clear();
+	}
+	cnt--;
+	if (cnt == -1) {
+		for (int i = 3; i >= 0; i--) {
+			for (int j = 0; j < 4; j++)
+				cout << eigenVector[i][j] << " ";
+			cout << "\n";
+		}
+		cout << "\n";
+	}
+}
+// print
+void printAMinus() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++)
+			cout << AMinusRamdaIdentity[i][j] << " ";
+		cout << "\n";
+	}
+	cout << "\n";
+}
+int main() {
+	setSystem();
+	// 사용자로부터 하나의 벡터와 알고싶은 지수승을 입력받는다.
+	double input[4];
+	for (int i = 0; i < 4; i++) {
+		double data; cin >> data;
+		input[i] = data;
+	}
+	int k; cin >> k;
+	setRamda();
+	for (int i = 0; i < ramda.size(); i++) {
+		// 1은 임의의 실수값을 의미한다.
+		setAMinusRamdaIdentity(ramda[i]);
+		rowEchelon(AMinusRamdaIdentity);
+		printAMinus();
+		calEigenVector(AMinusRamdaIdentity);
+	}
 }
